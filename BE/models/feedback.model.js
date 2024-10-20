1744a65b3686eff6574898ef041ab67667c2ff5f
Areeb Ahmad <areebahmad1503@gmail.com>
2024-10-20 15:57:16 +0530

    package added

---
+++ BE/models/feedback.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const feedbackSchema = new mongoose.Schema({
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User', // Reference to the User model
+      required: true
+    },
+    feedbackText: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true
+    },
+    comments: {
+      type: String,
+      trim: true
+    } 
+   
+  },{timestamps:true});
+  
+  export const Feedback = mongoose.model('Feedback', feedbackSchema);

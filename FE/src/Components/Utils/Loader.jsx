9ed9df0950fd124a312144d6fffa5758ef7b95ec
Areeb Ahmad <areebahmad1503@gmail.com>
2026-04-07 18:02:58 +0530

    loader changed

--- FE/src/Components/Utils/Loader.jsx
+++ FE/src/Components/Utils/Loader.jsx
@@ -2,13 +2,10 @@ import React from "react";
 
 const Loader = ({ fullScreen = true }) => {
   return (
-    <div
-      className={`flex items-center justify-center ${
-        fullScreen ? "h-screen" : "h-full"
-      }`}
-    >
-      <div className="w-12 h-12 border-4 border-orange-500 border-t-transparent rounded-full animate-spin"></div>
-    </div>
+   <div className="flex justify-center items-center min-h-screen">
+            {/* Tailwind Spinner */}
+            <div className="w-16 h-16 border-4 border-orange-700 border-dashed rounded-full animate-spin"></div>
+          </div>
   );
 };
 
